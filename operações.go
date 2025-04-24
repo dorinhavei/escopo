a4286c47bfca2f858fc88cd76770090778b1576c
isa.dora.farias4601@gmail.com <isa.dora.farias4601@gmail.com>
2025-04-24 15:59:29 -0300

    firstcommit

---
+++ operações.go
@@ -0,0 +1,17 @@
+package main
+
+func soma(x, y int) int{
+    return x + y
+}
+
+func subtraição(x, y int) int{
+    return x - y
+}
+
+func multiplicacao(x, y int) int{
+    return x * y
+}
+
+func division(x, y int) int{
+	return x / y
+}
